742c2f2daf68f619cbcae5c4829cc3907fcd3a7a
Sven A. Schmidt <sas@feinstruktur.com>
2015-04-11 09:44:36 +0200

    Initial Commit

---
+++ CoPilotPlugin/CoPilotPlugin.swift
@@ -0,0 +1,48 @@
+//
+//  CoPilotPlugin.swift
+//
+//  Created by Sven Schmidt on 11/04/2015.
+//  Copyright (c) 2015 feinstruktur. All rights reserved.
+//
+
+import AppKit
+
+var sharedPlugin: CoPilotPlugin?
+
+class CoPilotPlugin: NSObject {
+    var bundle: NSBundle
+
+    class func pluginDidLoad(bundle: NSBundle) {
+        let appName = NSBundle.mainBundle().infoDictionary?["CFBundleName"] as? NSString
+        if appName == "Xcode" {
+            sharedPlugin = CoPilotPlugin(bundle: bundle)
+        }
+    }
+
+    init(bundle: NSBundle) {
+        self.bundle = bundle
+
+        super.init()
+        createMenuItems()
+    }
+
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+
+    func createMenuItems() {
+        var item = NSApp.mainMenu!!.itemWithTitle("Edit")
+        if item != nil {
+            var actionMenuItem = NSMenuItem(title:"Do Action", action:"doMenuAction", keyEquivalent:"")
+            actionMenuItem.target = self
+            item!.submenu!.addItem(NSMenuItem.separatorItem())
+            item!.submenu!.addItem(actionMenuItem)
+        }
+    }
+
+    func doMenuAction() {
+        let error = NSError(domain: "Hello World!", code:42, userInfo:nil)
+        NSAlert(error: error).runModal()
+    }
+}
+
