11e1868ee0359510c1daa5caecbcf4eb359cc607
Sven A. Schmidt <sas@feinstruktur.com>
2015-04-21 16:22:26 +0100

    WIP

--- CoPilotPlugin/CoPilotPlugin-Bridging-Header.h
+++ CoPilotPlugin/CoPilotPlugin-Bridging-Header.h
@@ -3,3 +3,4 @@
 //
 
 #import "DiffMatchPatch.h"
+//#import "DTXcodeHeaders.h"
--- CoPilotPlugin/CoPilotPlugin.swift
+++ CoPilotPlugin/CoPilotPlugin.swift
@@ -8,6 +8,43 @@
 import AppKit
 import Cocoa
 
+
+//var currentWindow: NSWindow? {
+//get {
+//    return NSApplication.sharedApplication().keyWindow
+//}
+//}
+//
+//
+//var currentWorkspaceWindowController: AnyObject? {
+//get {
+////    let type: AnyClass! = NSClassFromString("IDEWorkspaceWindowController")
+//    return currentWindow?.windowController() // as? type.dynamicType
+//}
+//}
+//
+//
+//var currentEditorArea: IDEEditorArea? {
+//get {
+//    return currentWorkspaceWindowController?.editorArea
+//}
+//}
+//
+//
+//var currentEditorContext: IDEEditorContext? {
+//get {
+//    return currentEditorArea?.lastActiveEditorContext
+//}
+//}
+//
+//
+//var currentEditor: IDEEditor? {
+//get {
+//    return currentEditorContext?.editor
+//}
+//}
+
+
 var sharedPlugin: CoPilotPlugin?
 
 class CoPilotPlugin: NSObject {
@@ -32,22 +69,45 @@ class CoPilotPlugin: NSObject {
         NSNotificationCenter.defaultCenter().removeObserver(self)
     }
 
+}
+
+
+// MARK: - Helpers
+extension CoPilotPlugin {
+    
     func createMenuItems() {
         var item = NSApp.mainMenu!!.itemWithTitle("Edit")
         if item != nil {
-            var actionMenuItem = NSMenuItem(title:"CoPilot", action:"doMenuAction", keyEquivalent:"x")
-            actionMenuItem.keyEquivalentModifierMask = Int((NSEventModifierFlags.ControlKeyMask | NSEventModifierFlags.CommandKeyMask).rawValue)
-            actionMenuItem.target = self
             item!.submenu!.addItem(NSMenuItem.separatorItem())
-            item!.submenu!.addItem(actionMenuItem)
+            item!.submenu!.addItem(menuItem("CoPilot Publish", action:"publish", key:"p"))
+            item!.submenu!.addItem(menuItem("CoPilot Browse", action:"browse", key:"x"))
         }
     }
 
-    func doMenuAction() {
+    
+    func menuItem(title: String, action: Selector, key: String) -> NSMenuItem {
+        let m = NSMenuItem(title: title, action: action, keyEquivalent: key)
+        m.keyEquivalentModifierMask = Int((NSEventModifierFlags.ControlKeyMask | NSEventModifierFlags.CommandKeyMask).rawValue)
+        m.target = self
+        return m
+    }
+    
+}
+
+
+// MARK: - Actions
+extension CoPilotPlugin {
+
+    func publish() {
+        
+    }
+    
+    func browse() {
         if self.mainController == nil {
             self.mainController = MainController(windowNibName: "MainController")
         }
         self.mainController?.showWindow(self)
     }
+    
 }
 
