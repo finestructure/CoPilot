ad9f91ca61f96a8d2d46aa739fd3300a9c88092d
Sven A. Schmidt <sas@feinstruktur.com>
2015-04-29 09:14:48 +0100

    Added DTXcodeUtils pod. NB: there’s no target dependency setup - the two libs need to be built manually

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :osx
+
+target 'CoPilotPlugin' do
+  pod "DTXcodeUtils"
+end
---
+++ Pods/Target Support Files/Pods-CoPilotPlugin/Pods-CoPilotPlugin-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// DTXcodeUtils
+#define COCOAPODS_POD_AVAILABLE_DTXcodeUtils
+#define COCOAPODS_VERSION_MAJOR_DTXcodeUtils 0
+#define COCOAPODS_VERSION_MINOR_DTXcodeUtils 1
+#define COCOAPODS_VERSION_PATCH_DTXcodeUtils 1
+
