8d227d8a1b2c880043ff59d89441548e5fedd331
Sven A. Schmidt <sas@feinstruktur.com>
2015-04-30 12:31:21 +0100

    Client-server test

--- CoPilotPluginTests/DocClientTests.swift
+++ CoPilotPluginTests/DocClientTests.swift
@@ -23,17 +23,52 @@ class DocClient {
 }
 
 
-class DocClientTests: XCTestCase {
+func startServer() -> Server {
+    let s = Server(name: "foo", service: CoPilotService)
+    var started = false
+    s.onPublished = { ns in
+        expect(ns).toNot(beNil())
+        started = true
+    }
+    s.start()
+    expect(started).toEventually(beTrue(), timeout: 5)
+    return s
+}
 
-    func test_server() {
-        let s = Server(name: "foo", service: CoPilotService)
-        var started = false
-        s.onPublished = { ns in
-            expect(ns).toNot(beNil())
-            started = true
-        }
-        s.start()
-        expect(started).toEventually(beTrue(), timeout: 5)
+
+class DocClientTests: XCTestCase {
+    var open = false
+
+    func test_client() {
+        let s = startServer()
+        let url = NSURL(string: "ws://localhost:\(CoPilotService.port)")!
+        let req = NSURLRequest(URL: url)
+        self.open = false
+        let socket = PSWebSocket.clientSocketWithRequest(req)
+        socket.delegate = self
+        socket.open()
+        expect(self.open).toEventually(beTrue(), timeout: 5)
+    }
+    
+}
+
+
+extension DocClientTests: PSWebSocketDelegate {
+    
+    func webSocketDidOpen(webSocket: PSWebSocket!) {
+        self.open = true
+    }
+    
+    func webSocket(webSocket: PSWebSocket!, didReceiveMessage message: AnyObject!) {
+        
+    }
+    
+    func webSocket(webSocket: PSWebSocket!, didCloseWithCode code: Int, reason: String!, wasClean: Bool) {
+        
+    }
+    
+    func webSocket(webSocket: PSWebSocket!, didFailWithError error: NSError!) {
+        
     }
     
 }
