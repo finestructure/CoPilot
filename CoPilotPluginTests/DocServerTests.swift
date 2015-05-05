9223f8bb9dd928f680435da38aa2e841a1376da6
Sven A. Schmidt <sas@feinstruktur.com>
2015-05-05 16:47:13 +0100

    Moved DocServer to own file, tests pass, plugin runs.

--- CoPilotPluginTests/DocServerTests.swift
+++ CoPilotPluginTests/DocServerTests.swift
@@ -11,57 +11,6 @@ import XCTest
 import Nimble
 import FeinstrukturUtils
 
-let PollInterval = 0.5
-
-typealias TextProvider = (Void -> String)
-
-class DocServer: NSObject {
-    
-    private let textProvider: TextProvider
-    private let server: Server
-    private var timer: NSTimer?
-    private var lastDoc: Document?
-    
-    init(name: String, service: BonjourService = CoPilotService, textProvider: TextProvider) {
-        self.textProvider = textProvider
-        self.server = Server(name: name, service: service)
-        self.server.start()
-        self.timer = nil
-        super.init()
-        let timer = NSTimer.scheduledTimerWithTimeInterval(PollInterval, target: self, selector: "pollProvider", userInfo: nil, repeats: true)
-        self.timer = timer
-    }
-
-    deinit {
-        self.stop()
-    }
-    
-    func stop() {
-        self.server.stop()
-    }
-    
-    func pollProvider() {
-        let newDoc = Document(self.textProvider())
-        
-        if newDoc.hash == self.lastDoc?.hash {
-            return
-        }
-        
-        let command: Command = {
-            if self.lastDoc == nil {
-                return Command(initialize: newDoc)
-            } else {
-                let changes = Changeset(source: self.lastDoc!, target: newDoc)
-                return Command(update: changes)
-            }
-        }()
-
-        self.server.broadcast(command.serialize())
-        self.lastDoc = newDoc
-    }
-    
-}
-
 
 let words = [
     "this",
