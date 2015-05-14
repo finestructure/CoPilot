df448f65de1e50667025637ee53176ed272b0bd4
Sven A. Schmidt <sas@feinstruktur.com>
2015-05-14 14:08:00 +0100

    Added table view

--- CoPilotPlugin/ConnectedController.swift
+++ CoPilotPlugin/ConnectedController.swift
@@ -10,10 +10,49 @@ import Cocoa
 
 class ConnectedController: NSWindowController {
 
+    @IBOutlet weak var tableView: NSTableView!
+    
     override func windowDidLoad() {
         super.windowDidLoad()
+    }
 
-        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    
+    override func awakeFromNib() {
+        self.updateUI()
+    }
+    
+}
+
+
+// MARK: - Helpers
+extension ConnectedController {
+    
+    func updateUI() {
+        self.tableView.reloadData()
+    }
+    
+}
+
+
+// MARK: - NSTableViewDataSource
+extension ConnectedController: NSTableViewDataSource {
+    
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        return 5
+    }
+    
+}
+
+
+// MARK: - NSTableViewDelegate
+extension ConnectedController: NSTableViewDelegate {
+    
+    func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let cell = tableView.makeViewWithIdentifier("ConnectionCell", owner: self) as? NSTableCellView
+
+        cell?.textField?.stringValue = "item \(row)"
+
+        return cell
     }
     
 }
