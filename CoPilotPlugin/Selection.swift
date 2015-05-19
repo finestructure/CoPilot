a6caa9af77125409c503fe0eb45d587f8d15529d
Sven A. Schmidt <sas@feinstruktur.com>
2015-05-19 19:55:58 +0100

    Added Selection

---
+++ CoPilotPlugin/Selection.swift
@@ -0,0 +1,34 @@
+//
+//  Selection.swift
+//  CoPilotPlugin
+//
+//  Created by Sven A. Schmidt on 19/05/2015.
+//  Copyright (c) 2015 feinstruktur. All rights reserved.
+//
+
+import Foundation
+
+
+struct Selection {
+    let range: NSRange
+
+    init(_ range: NSRange) {
+        self.range = range
+    }
+
+    init(data: NSData) {
+        let decoder = NSKeyedUnarchiver(forReadingWithData: data)
+        let value = decoder.decodeObjectForKey("range") as! NSValue
+        self.range = value.rangeValue
+    }
+
+    func serialize() -> NSData {
+        let data = NSMutableData()
+        let archiver = NSKeyedArchiver(forWritingWithMutableData: data)
+        let value = NSValue(range: self.range)
+        archiver.encodeObject(value, forKey: "range")
+        archiver.finishEncoding()
+        return data
+    }
+
+}
