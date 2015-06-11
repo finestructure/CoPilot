d185f8d4c37f30106b6bf125e6dcd2ba19b66f8a
Sven A. Schmidt <sas@feinstruktur.com>
2015-06-11 10:35:21 +0100

    Unzip doesn't support stdin

--- Misc/install.sh
+++ Misc/install.sh
@@ -2,9 +2,13 @@
 
 DOWNLOAD_URI=https://github.com/feinstruktur/CoPilot/releases/download/0.11/CoPilot.xcplugin-0.11.zip
 PLUGINS_DIR="${HOME}/Library/Application Support/Developer/Shared/Xcode/Plug-ins"
+FNAME=dl.zip
 
 mkdir -p "${PLUGINS_DIR}"
-curl -L $DOWNLOAD_URI | unzip -d "${PLUGINS_DIR}"
+cd "${PLUGINS_DIR}"
+curl -L $DOWNLOAD_URI -o $FNAME
+unzip $FNAME
+rm -rf $FNAME
 
 # the 1 is not a typo!
 echo "CoPilot successfully installed!!1!🍻   Please restart your Xcode."
