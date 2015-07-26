79a84fb93207f4c0f5eb293f20dd1348497706c6
Sven A. Schmidt <sas@feinstruktur.com>
2015-07-26 11:25:02 +0100

    WIP multiple subscribers. Comm doesn’t appear to work bi-directionally at the moment. Only the server comms go to the clients, not vice-versa.
    /Users/sas/Projects/CoPilot/CoPilotTests/CPServerTests.swift:79: error: -[CoPilotTests.CPServerTests test_two_subscribers] : failed - expected to eventually equal <2>, got <1>
    
    /Users/sas/Projects/CoPilot/CoPilotTests/CPServerTests.swift:81: error: -[CoPilotTests.CPServerTests test_two_subscribers] : failed - expected to eventually equal <2>, got <1>
    
    /Users/sas/Projects/CoPilot/CoPilotTests/CPServerTests.swift:83: error: -[CoPilotTests.CPServerTests test_two_subscribers] : failed - expected to eventually equal <1>, got <0>
    
    Test Case '-[CoPilotTests.CPServerTests test_two_subscribers]' failed (3.084 seconds).
    Test Suite 'CPServerTests' failed at 2015-07-25 12:32:57.869.
    	 Executed 1 test, with 3 failures (0 unexpected) in 3.084 (3.085) seconds
    Test Suite 'Selected tests' failed at 2015-07-25 12:32:57.870.
    	 Executed 1 test, with 3 failures (0 unexpected) in 3.084 (3.086) seconds
    Program ended with exit code: 1

--- CoPilotTests/CPServerTests.swift
+++ CoPilotTests/CPServerTests.swift
@@ -10,16 +10,22 @@ import XCTest
 import Nimble
 
 
-let GoPublishUrl = NSURL(string: "ws://localhost:12345/doc/1/publish")!
-let GoSubscribeUrl = NSURL(string: "ws://localhost:12345/doc/1/subscribe")!
+func publishUrl(docId: String) -> NSURL {
+    return NSURL(string: "ws://localhost:12345/doc/\(docId)/publish")!
+}
+
+
+func subscribeUrl(docId: String) -> NSURL {
+    return NSURL(string: "ws://localhost:12345/doc/\(docId)/subscribe")!
+}
 
 
 class CPServerTests: XCTestCase {
 
     func test_cpserver() {
-        let s = connectWebsocket(GoPublishUrl)
+        let s = connectWebsocket(publishUrl("1"))
         expect(s).toNot(beNil())
-        let c = connectWebsocket(GoSubscribeUrl)
+        let c = connectWebsocket(subscribeUrl("1"))
         expect(c).toNot(beNil())
         var message: Message?
         c.onReceive = { msg in
@@ -34,6 +40,45 @@ class CPServerTests: XCTestCase {
         s.send(Command(name: "server"))
         expect(message).toEventuallyNot(beNil())
         print("message: \(message)")
+    }
+
+
+    func test_two_subscribers() {
+        let s = connectWebsocket(publishUrl("2"))
+        let c1 = connectWebsocket(subscribeUrl("2"))
+        let c2 = connectWebsocket(subscribeUrl("2"))
+
+        var sMsg = [Message]()
+        s.onReceive = { msg in
+            print("s: \(msg)")
+            sMsg.append(msg)
+        }
+
+        var c1Msg = [Message]()
+        c1.onReceive = { msg in
+            print("c1: \(msg)")
+            c1Msg.append(msg)
+        }
+
+        var c2Msg = [Message]()
+        c2.onReceive = { msg in
+            print("c2: \(msg)")
+            c2Msg.append(msg)
+        }
+
+        s.send(Command(name: "server"))
+
+        expect(c1Msg.count).toEventually(equal(1))
+        expect(Command(data: c1Msg[0].data!).name) == "server"
+        expect(c2Msg.count).toEventually(equal(1))
+        expect(Command(data: c2Msg[0].data!).name) == "server"
+        expect(sMsg.count) == 0
+
+        c1.send(Command(name: "c1"))
+
+        expect(c1Msg.count) == 1
+        expect(c2Msg.count).toEventually(equal(2))
+        expect(sMsg.count).toEventually(equal(1))
     }
 
 }
