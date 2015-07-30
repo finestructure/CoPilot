9ded524cd6cbf3d9005e2015bd842dd570529496
Sven A. Schmidt <sas@feinstruktur.com>
2015-07-30 11:57:23 +0200

    Added DocumentService protocol

---
+++ CoPilot/DocumentService.swift
@@ -0,0 +1,26 @@
+//
+//  DocumentService.swift
+//  CoPilot
+//
+//  Created by Sven A. Schmidt on 30/07/2015.
+//  Copyright © 2015 feinstruktur. All rights reserved.
+//
+
+import Foundation
+
+
+typealias ConnectionId = String
+typealias ClientHandler = (ConnectionId -> Void)
+
+
+protocol DocumentService {
+
+    func publish(name: String) -> ConnectionId
+    func unpublish(id: ConnectionId)
+    var onClientConnect: ClientHandler? { get set }
+    var onClientDisconnect: ClientHandler? { get set }
+    func broadcast(message: Message, except: [ConnectionId])
+    func send(message: Message, receiver to: ConnectionId)
+
+}
+
