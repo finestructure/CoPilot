ee0af38937c985979507258b9ee0491721f45227
Sven A. Schmidt <sas@feinstruktur.com>
2015-10-13 17:26:07 +0100

    Working rabbit based test

--- CoPilotTests/CPServerTests.swift
+++ CoPilotTests/CPServerTests.swift
@@ -9,6 +9,7 @@
 import XCTest
 import Nimble
 import HastyHare
+import Async
 
 
 func docUrl(docId: String) -> NSURL {
@@ -28,7 +29,96 @@ extension Message {
 }
 
 
-class CPServerTests: XCTestCase {
+
+class RabbitSocket: Socket {
+    let id: ConnectionId
+
+    private var connection: Connection?
+    private var channel: Channel?
+    private var consumer: Consumer?
+    private var exchange: Exchange?
+
+    init(connectionId: ConnectionId) {
+        self.id = connectionId
+    }
+
+    func open() {
+        self.connection = self.connect()
+        self.channel = self.connection?.openChannel()
+        self.exchange = self.channel?.declareExchange("doc_exchange")
+        let q = self.channel?.declareQueue(self.id)
+        q?.bindToExchange("doc_exchange", bindingKey: self.id)
+        self.onConnect?()
+    }
+
+    func close() {
+        self.channel = nil
+        self.connection = nil
+        self.onDisconnect?(nil)
+    }
+
+    func send(message: Message) {
+        switch message {
+        case .Text(let s):
+            self.exchange?.publish(s, routingKey: self.id)
+        case .Data(let d):
+            self.exchange?.publish(d, routingKey: self.id)
+        }
+    }
+
+    var onConnect: ConnectionHandler?
+    var onDisconnect: ErrorHandler?
+    var onReceive: MessageHandler? {
+        didSet {
+            self.consumer = self.channel?.consumer(self.id)
+            // FIXME: this needs to be more than just a one-off
+            Async.background {
+                if let s = self.consumer?.pop() {
+                    let msg = Message(s)
+                    self.onReceive?(msg)
+                }
+            }
+        }
+    }
+
+    func connect() -> Connection {
+        let hostname = "dockerhost"
+        let port = 5672
+        let username = "guest"
+        let password = "guest"
+        let c = Connection(host: hostname, port: port)
+        c.login(username, password: password)
+        return c
+    }
+}
+
+
+func connectDoc(connectionId: ConnectionId) -> Socket {
+    let s = RabbitSocket(connectionId: connectionId)
+    s.open()
+    return s
+}
+
+
+class RabbitServerTests: XCTestCase {
+
+    func test_cpserver() {
+        do { // document host
+            let s = connectDoc("doc1")
+            s.send("ping")
+        }
+        do { // document client
+            var s = connectDoc("doc1")
+
+            var msg: String?
+            s.onReceive = { m in
+                msg = m.string
+            }
+            expect(msg).toEventuallyNot(beNil(), timeout: 2)
+            expect(msg) == Optional("ping")
+        }
+    }
+
 
     func _test_cpserver() {
         let s = connectWebsocket(docUrl("1"))
