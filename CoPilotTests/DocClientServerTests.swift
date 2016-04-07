be3670df1890c8f9aa6763b49cf6343365a1c855
Sven A. Schmidt <sas@feinstruktur.com>
2016-04-07 10:56:04 +0200

    Fixed swift-2.2 warnings and fixits. Tests pass, although there was once a race condition in `test_config_client_update`, see issue #47

--- CoPilotTests/DocClientServerTests.swift
+++ CoPilotTests/DocClientServerTests.swift
@@ -77,7 +77,7 @@ class DocClientServerTests: XCTestCase {
         
         let client = DocClient(service: service, document: Document(""))
         var changeCount = 0
-        client.onDocumentUpdate = { _ in changeCount++ }
+        client.onDocumentUpdate = { _ in changeCount += 1 }
         expect(changeCount).toEventually(beGreaterThan(0), timeout: 5)
     }
 
@@ -91,7 +91,7 @@ class DocClientServerTests: XCTestCase {
 
         let client = DocClient(url: url, document: Document(""))
         var changeCount = 0
-        client.onDocumentUpdate = { _ in changeCount++ }
+        client.onDocumentUpdate = { _ in changeCount += 1 }
         expect(changeCount).toEventually(beGreaterThan(0), timeout: 5)
     }
 
