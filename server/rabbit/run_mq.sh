2bdf1f49e43f5786ee9156712d58209d04544323
Sven A. Schmidt <sas@feinstruktur.com>
2016-06-10 15:58:48 +0100

    Updated dependencies and run script Nimble 4.0.1 HastyHare 0.1

--- server/rabbit/run_mq.sh
+++ server/rabbit/run_mq.sh
@@ -2,4 +2,4 @@
 docker stop some-rabbit
 docker rm some-rabbit
 docker run -d --hostname my-rabbit --name some-rabbit -p 5672:5672 -p 8080:15672 rabbitmq:3-management
-echo open "http://dockerhost:8080"
+echo open "http://localhost:8080"
